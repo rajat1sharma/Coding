399b7dfdfc3f152fdefef3ee9add3fad5c6f21d8
rajat <rajat.sharma4211@gmail.com>
2020-03-25 17:37:11 +0530

    okay

---
+++ Codechef/DELISH.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+#define ll long long
+#define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define re(i,n) for(ll i = 0 ; i < n ; ++i)
+#define ree(n) for(ll i = 0 ; i < n ; i++)
+#define rep(i , j , n) for(ll i = j ; i < n ; i++)
+#define per(i , j , n) for(ll i = j ; i >= n ; i--)
+#define mod 1000000007
+/*
+#ifndef ONLINE_JUDGE
+    freopen("input22.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+*/
+int main()
+{
+    SPEED;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	#endif
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		ll ans=1;
+		rep(i,1,sqrt(n)+1)
+		{
+			if(n%i==0)
+				ans=i;
+		}
+		cout<<abs(ans-(n/ans))<<endl;
+	}
+
+}
